0af4daa88b2367f0c9ce3b872b9c22f92fdd4c73
amirakbarwicaksono <67996818+amirakbarwicaksono@users.noreply.github.com>
2025-01-14 15:36:43 +0700

    Update authenticate.ts

--- src/pages/api/authenticate.ts
+++ src/pages/api/authenticate.ts
@@ -4,7 +4,7 @@ import * as cookie from 'cookie';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const { password } = req.body;
-        const correctPassword = 'bagas';
+        const correctPassword = 'password';
 
         if (password === correctPassword) {
             res.setHeader(
