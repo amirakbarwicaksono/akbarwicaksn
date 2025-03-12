b0b55874330144585defc05a7bc192979feb7321
Akbarwicaksn <67996818+amirakbarwicaksono@users.noreply.github.com>
2025-03-12 09:17:02 +0700

    Update config.js
    added other blog for protected

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -20,8 +20,8 @@ const routes = {
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-    '/work/building-once-ui-a-customizable-design-system': true
-    '/work/automate-design-handovers-with-a-figma-to-code-pipeline' : true
+    '/work/building-once-ui-a-customizable-design-system': true,
+    '/work/automate-design-handovers-with-a-figma-to-code-pipeline' : true,
     '/work/simple-portfolio-builder' : true
 }
 
