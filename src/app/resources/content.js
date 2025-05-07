c046163bdf4594c4edc9fd62ebf46e7063e3fbd7
Akbarwicaksn <67996818+amirakbarwicaksono@users.noreply.github.com>
2025-05-07 15:06:56 +0700

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -151,19 +151,19 @@ const about = {
                 // optional: leave the array empty if you don't want to display images
                 images: [
                     {
-                        src: '/images/projects/project-01/cover-06.jpeg',
+                        src: '/images/projects/project-01/cover-06.jpg',
                         alt: 'Project image',
                         width: 16,
                         height: 9
                     },
                     {
-                        src: '/images/projects/project-01/cover-07.jpeg',
+                        src: '/images/projects/project-01/cover-07.jpg',
                         alt: 'Project image',
                         width: 16,
                         height: 9
                     },
                     {
-                        src: '/images/projects/project-01/cover-08.jpeg',
+                        src: '/images/projects/project-01/cover-08.jpg',
                         alt: 'Project image',
                         width: 16,
                         height: 9
