bc07f78f2457500eb0e9fe255b0d9f988f94ae9e
Firas Budi <firasbudi@Firass-MacBook-Air.local>
2026-01-05 13:56:43 +0700

    Change About

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -100,11 +100,17 @@ const about: About = {
   intro: {
     display: true,
     title: "Introduction",
+    // description: (
+    //   <>
+    //     Akbar is a Jakarta-based lecturer at Universitas Trisakti, dedicated to advancing the Tridarma Perguruan Tinggi through innovative teaching, cutting-edge research, and community engagement. His work seamlessly integrates education with pioneering research in Drones, Internet of Things (IoT), and the intersection of technology and creative arts, transforming complex technical challenges into impactful solutions.
+    //   </>
+    // ),
     description: (
       <>
-        Akbar is a Jakarta-based lecturer at Universitas Trisakti, dedicated to advancing the Tridarma Perguruan Tinggi through innovative teaching, cutting-edge research, and community engagement. His work seamlessly integrates education with pioneering research in Drones, Internet of Things (IoT), and the intersection of technology and creative arts, transforming complex technical challenges into impactful solutions.
+        Akbar is a Jakarta-based lecturer at <strong>Universitas Trisakti</strong>, driven by a commitment to strengthening scientific literacy through the Tridarma Perguruan Tinggi, encompassing teaching, research, and community engagement. His academic work focuses on Internet of Things (IoT), embedded systems, machine learning, and unmanned aerial vehicles (UAVs), aiming to bridge fundamental science with practical and accessible technologies. Through his teaching and research, he seeks to translate complex technical challenges into meaningful solutions that inspire students and create tangible impact for society.
       </>
-    ),
+     ),
+
   },
   work: {
     display: true, // set to false to hide this section
